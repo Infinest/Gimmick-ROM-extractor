e593a401016c6669a26d04d3627e27e93efa1879
Infinest <nicon155@yahoo.de>
2023-12-19 21:15:13 +0100

    Fix signature

--- Gimmick ROM extractor/Program.cs
+++ Gimmick ROM extractor/Program.cs
@@ -198,10 +198,9 @@ namespace Gimmick_ROM_extractor
         /// </summary>
         private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
         {
-            var executingAssembly = Assembly.GetExecutingAssembly();
             var assemblyName = new AssemblyName(args.Name);
-
             var path = assemblyName.Name + ".dll";
+            var executingAssembly = Assembly.GetExecutingAssembly();
             if (!assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture))
             {
                 path = $"{assemblyName.CultureInfo}\\${path}";
